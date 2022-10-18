c88391627a2bace9c1d1bfd95e32f92778e5479d
Jonathan <jonathan.gabioud@hotmail.com>
2022-10-18 23:14:08 +0200

    Cells updates

--- src/Main.java
+++ src/Main.java
@@ -2,15 +2,14 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        int i, j, l = 0, c = 0;
+        int l = 0, c = 0;
         char maze[][];
-        Scanner sc = new Scanner(System.in);
-
-
-        System.out.println("How many lines");
-        l = sc.nextInt();
-        System.out.println("How many columns");
-        c = sc.nextInt();
+        try (Scanner sc = new Scanner(System.in)) {
+            System.out.println("How many lines");
+            l = sc.nextInt();
+            System.out.println("How many columns");
+            c = sc.nextInt();
+        }
 
         maze = new char[l][c];
 
@@ -20,11 +19,11 @@ public class Main {
     public static void Initialize(int l,int c, char t[][]) {
         for(int i = 0; i < l; i++) {
             for(int j = 0; j < c; j++) {
-                System.out.print(((t[i][j] & 1) == 0) ? "+---" : "    ");
+                System.out.print((t[i][j] == 0) ? "+---" : "    ");
             }
             System.out.println("+");
             for(int j = 0; j < c; j++) {
-                System.out.print(((t[i][j] & 1) == 0) ? "|   " : "    ");
+                System.out.print((t[i][j] == 0) ? "|   " : "    ");
             }
             System.out.println("|");
         }
