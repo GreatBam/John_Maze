2e0111d105c475bd0e056ae60e106dbb80843bf8
Jonathan <jonathan.gabioud@hotmail.com>
2022-10-19 00:09:50 +0200

    Improve integers check

--- src/Main.java
+++ src/Main.java
@@ -1,40 +1,56 @@
 import java.util.Scanner;
+import java.util.InputMismatchException;
 
 public class Main {
     public static void main(String[] args) {
-        int lines = 0, columns = 0;
-        char maze[][];
-        Scanner sc = new Scanner(System.in);
+        int lines = 0, columns = 0; char maze[][]; Scanner sc = new Scanner(System.in);
 
-        System.out.print("How many lines: ");
-        // checking for integer         
-        while (!sc.hasNextInt())
-        {
-        System.out.println("Please Enter integer!");
-        sc.nextLine();
+        // checking for integers
+        while(true) {
+            try {
+                System.out.print("How many lines: ");
+                lines = sc.nextInt();
+                if((lines >= 0 && lines <= Integer.MAX_VALUE)) {
+                    System.out.println("Set");
+                    System.out.println(" ");
+                    break;
+                }
+            }
+            catch(InputMismatchException ex) {
+                System.out.println("Error : invalid value");
+                System.out.println(" ");
+                String s = sc.next();
+            }           
         }
-        lines = Integer.parseInt(sc.nextLine());
-
-        System.out.print("How many columns: ");
-        //checking for integer           
-        while (!sc.hasNextInt())
-        {
-        System.out.println("Please Enter integer!");
-        sc.nextLine();
+        while(true) {
+            try {
+                System.out.print("How many columns: ");
+                columns = sc.nextInt();
+                if((columns >= 0 && columns <= Integer.MAX_VALUE)) {
+                    System.out.println("Set");
+                    System.out.println(" ");
+                    break;
+                }
+            }
+            catch(InputMismatchException ex) {
+                System.out.println("Error : invalid value");
+                System.out.println(" ");
+                String s = sc.next();
+            }           
         }
-        columns = Integer.parseInt(sc.nextLine());
-
-        // close Scanner
+        System.out.println("Lines   : " + lines);
+        System.out.println("Columns : " + columns);
+        System.out.println(" ");
         sc.close();
 
         // size maze
         maze = new char[lines][columns];
 
-        // print maze
-        Initialize(lines, columns, maze);
+        // display maze
+        display(lines, columns, maze);
     }
 
-    public static void Initialize(int lines,int columns, char maze[][]) {
+    public static void display(int lines,int columns, char maze[][]) {
         for(int i = 0; i < lines; i++) {
 
             // creating north walls
@@ -50,7 +66,7 @@ public class Main {
             System.out.println("|");
         }
 
-        // bottom line
+        // creating bottom line
         for (int j = 0; j < columns; j++) {
 			System.out.print("+---");
 		}
